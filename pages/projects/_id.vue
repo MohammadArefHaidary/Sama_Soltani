9bb76e49ff3837521054a77407798747cfe03f82
Mohammad Aref <mohammadaref.haidary1234@gmail.com>
2022-11-23 14:58:13 +0330

    fix:modal created

--- pages/projects/_id.vue
+++ pages/projects/_id.vue
@@ -2,11 +2,20 @@
 import feather from "feather-icons";
 import ProjectRelatedProjects from "../../components/projects/ProjectRelatedProjects.vue";
 export default {
+  name: "regular-modal",
   scrollToTop: true,
   data: () => {
     return {
       // @todo
+      showModal: false,
+      image: null,
     };
+  },
+  methods: {
+    toggleModal: function (data) {
+      this.image = data;
+      this.showModal = !this.showModal;
+    },
   },
   computed: {
     project() {
@@ -89,21 +98,60 @@ export default {
           v-for="projectImage in project.projectImages"
           :key="projectImage.id"
         >
-          <img
-            :src="projectImage.img"
-            class="rounded-xl cursor-pointer shadow-lg sm:shadow-none 
-           "
-          />
+         <button  type="button" v-on:click="toggleModal(projectImage.img)">
+            <img
+              :src="projectImage.img"
+              class="rounded-xl cursor-pointer shadow-lg sm:shadow-none 
+            "
+            />
+          </button>
            <h3 class="  text-primary-dark
-        dark:text-primary-light">{{projectImage.imgName}}</h3>
+                dark:text-primary-light">{{projectImage.imgName}}</h3>
            <div class="focus-content">
-          <p>{{projectImage.title}}<br/> 
-          {{projectImage.size}}<br/> 
-          <a href="#">Link to source</a>
-          </p>
-        </div>
+              <p>{{projectImage.title}}<br/> 
+              {{projectImage.size}}<br/> 
+              <a href="#">Link to source</a>
+              </p>
+          </div>
+      </div>
+
+<div>
+    <div v-if="showModal" class="overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none justify-center items-center flex">
+      <div class="relative w-auto my-6 mx-auto max-w-3xl">
+        <!--content-->
+        <div class="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+          <!--header-->
+          <div class="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+            <h3 class="text-3xl font-semibold">
+              {{image}}
+            </h3>
+            <button  class="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none" v-on:click="toggleModal()">
+              <span class="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                ×
+              </span>
+            </button>
+          </div>
+          <!--body-->
+          <div class="relative p-6 flex-auto">
+           <img :src="image" alt="">
+          </div>
+          <!--footer-->
+          <div class="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+            <button class="text-red-500 bg-transparent border border-solid border-red-500 hover:bg-red-500 hover:text-white active:bg-red-600 font-bold uppercase text-sm px-6 py-3 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" v-on:click="toggleModal()">
+              Close
+            </button>
+            <button class="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" v-on:click="toggleModal()">
+              Save Changes
+            </button>
+          </div>
         </div>
       </div>
+    </div>
+    <div v-if="showModal" class="opacity-25 fixed inset-0 z-40 bg-black"></div>
+  </div>
+        </div>
+            
+
 
       <!-- Project info -->
       <div class="block sm:flex gap-0 sm:gap-10 mt-14">
